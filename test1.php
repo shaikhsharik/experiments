5e6ffd0fbc996f2ebad626f55e3eba3154b16144
shaikhsharik <ershaikhsharik@gmail.com>
2015-01-12 11:17:26 +0530

    [deploy: production]

---
+++ test1.php
@@ -0,0 +1,23 @@
+<img src="http://cloverrealty.in/sites/all/themes/clover/images/clover-mail-logo.jpg" />
+<p style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif; font-size: 14px;"><u><b>Project Enquiry for Clover Realty:</b></u></p>
+<table>
+<tr>
+<td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"><b>Name</b></td><td>&nbsp;:&nbsp;</td><td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"> %value[name]</td>
+</tr>
+<tr><td></td><td></td><td></td></tr>
+<tr>
+<td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"><b>Phone</b></td><td>&nbsp;:&nbsp;</td><td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"> %value[phone_number]</td>
+</tr>
+<tr><td></td><td></td><td></td></tr>
+<tr>
+<td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"><b>Email</b></td><td>&nbsp;:&nbsp;</td><td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"> %value[email_id]</td>
+</tr>
+<tr><td></td><td></td><td></td></tr>
+<tr>
+<td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"><b>Budget</b></td><td>&nbsp;:&nbsp;</td><td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"> %value[budget]</td>
+</tr>
+<tr><td></td><td></td><td></td></tr>
+<tr>
+<td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"><b>Project</b></td><td>&nbsp;:&nbsp;</td><td style="color:#5e5c5c; font-family: TimesNewRoman,'Times New Roman', Times, Baskerville, Georgia, serif;"> %value[project]</td>
+</tr>
+</table>
